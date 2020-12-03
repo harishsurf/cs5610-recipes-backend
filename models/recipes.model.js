31d7a064271b7ff5da679a80b293a707bda384f6
Elaine Parr <parr.mi@husky.neu.edu>
2020-12-03 18:17:16 -0500

    start on recipes

--- models/recipes.model.js
+++ models/recipes.model.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 let recipeSchema = new mongoose.Schema({
     recipe_name: String,
+    recipe_id: int,
+    image_url: String,
+    image_type: String,
     ingredients: String,
-    tags : {type: String, enum: ["enum1", "enum2"]},
+    instructions: String,
+    ready_in_minutes: int,
+    servings: int,
+    source_url: String,
     user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}
 }, {collection: 'recipes'});
 
